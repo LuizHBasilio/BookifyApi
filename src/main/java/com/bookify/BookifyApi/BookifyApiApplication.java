aee5c47183a8961fa474a9c5f341b62d685f7286
zuiioll <luizbasilio@fitec.org.br>
2023-02-19 08:34:57 -0300

    Starting project

---
+++ src/main/java/com/bookify/BookifyApi/BookifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.bookify.BookifyApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookifyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookify/BookifyApi/BookifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookify.BookifyApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
