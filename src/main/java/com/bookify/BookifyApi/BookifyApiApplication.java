871563d8361f5687c3b1752cd8c6fa15d4b401ca
zuiioll <luizbasilio@fitec.org.br>
2023-02-28 16:26:48 -0300

    Added Javadoc in application and service classes

--- src/main/java/com/bookify/BookifyApi/BookifyApiApplication.java
+++ src/main/java/com/bookify/BookifyApi/BookifyApiApplication.java
@@ -4,14 +4,12 @@ import java.util.List;
 
 import main.java.com.bookify.BookifyApi.model.Book;
 import main.java.com.bookify.BookifyApi.service.BookService;
-import net.bytebuddy.agent.VirtualMachine.ForHotSpot.Connection.Response;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,58 +17,101 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The BookifyApiApplication class provides a RESTful API for managing a
+ * collection of books. It defines endpoints for retrieving all books,
+ * retrieving a specific book by ID, adding a new book, updating an existing
+ * book, and deleting a book.
+ */
 @SpringBootApplication
 @RestController
 @RequestMapping("/bookify/api/v1/books")
 public class BookifyApiApplication {
-	
+
+	/**
+	 * The BookService instance used to perform CRUD operations on the Book objects.
+	 */
 	static BookService bookService = new BookService();
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookifyApiApplication.class, args);
 	}
-	
+
+	/**
+	 * Retrieves all books.
+	 * 
+	 * @return A List of Book objects representing all books in the collection.
+	 */
 	@GetMapping
 	public List<Book> getAllBooks() {
 		return bookService.getAllBooks();
 	}
-	
+
+	/**
+	 * Retrieves a specific book by ID.
+	 * 
+	 * @param bookId The ID of the book to retrieve.
+	 * @return A ResponseEntity with a Book object if the book is found, or a 404
+	 *         status if it is not.
+	 */
 	@GetMapping("/{bookId}")
-	public ResponseEntity<Book> getBook(@PathVariable String bookId) {
-		Book book = bookService.getBookById(bookId);		
+	public ResponseEntity<Book> getBook(@PathVariable Long bookId) {
+		Book book = bookService.getBookById(bookId);
 		if (book == null) {
 			return ResponseEntity.notFound().build();
 		} else {
 			return ResponseEntity.ok(book);
 		}
 	}
-	
+
+	/**
+	 * Adds a new book.
+	 * 
+	 * @param book The Book object representing the book to add.
+	 * @return A ResponseEntity containing the added Book object if the ID is
+	 *         unique, or a 400 status if it already exists.
+	 */
 	@PostMapping
 	public ResponseEntity<Book> addBook(@RequestBody Book book) {
 		boolean keyExists = bookService.keyAlreadyExists(book.getId());
 		if (keyExists) {
 			return ResponseEntity.badRequest().build();
 		}
-		return ResponseEntity.ok(bookService.addBook(book));		
+		return ResponseEntity.ok(bookService.addBook(book));
 	}
-	
+
+	/**
+	 * Updates an existing book.
+	 * 
+	 * @param bookId The unique identifier of the book to update.
+	 * @param book   The Book object representing the updated book.
+	 * @return A ResponseEntity containing the updated Book object if it exists, or
+	 *         a 404 status if it does not.
+	 */
 	@PutMapping("/{bookId}")
 	public ResponseEntity<Book> updateBook(@PathVariable Long bookId, @RequestBody Book book) {
-	    boolean keyExists = bookService.keyAlreadyExists(bookId);
-	    if (!keyExists) {
-	        return ResponseEntity.notFound().build();
-	    }
-	    book.setId(bookId);
-	    return ResponseEntity.ok(bookService.updateBook(book));
+		boolean keyExists = bookService.keyAlreadyExists(bookId);
+		if (!keyExists) {
+			return ResponseEntity.notFound().build();
+		}
+		book.setId(bookId);
+		return ResponseEntity.ok(bookService.updateBook(book));
 	}
-	
+
+	/**
+	 * Deletes a book.
+	 * 
+	 * @param bookId The unique identifier of the book to delete.
+	 * @return A ResponseEntity with no content if the book is deleted, or a 404
+	 *         status if it does not exist.
+	 */
 	@DeleteMapping("/{bookId}")
 	public ResponseEntity<Void> deleteBook(@PathVariable Long bookId) {
-		 boolean keyExists = bookService.keyAlreadyExists(bookId);
-		    if (!keyExists) {
-		        return ResponseEntity.notFound().build();
-		    }
-		    bookService.deleteBook(bookId);
-		    return ResponseEntity.noContent().build();
+		boolean keyExists = bookService.keyAlreadyExists(bookId);
+		if (!keyExists) {
+			return ResponseEntity.notFound().build();
+		}
+		bookService.deleteBook(bookId);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/bookify/BookifyApi/service/BookService.java
+++ src/main/java/com/bookify/BookifyApi/service/BookService.java
@@ -1,7 +1,5 @@
 package main.java.com.bookify.BookifyApi.service;
 
-import static org.mockito.ArgumentMatchers.notNull;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,11 +7,18 @@ import java.util.Map;
 
 import main.java.com.bookify.BookifyApi.model.Book;
 
+/**
+ * This class represents a service layer for the Bookify API application. It
+ * manages the CRUD (Create, Read, Update, Delete) operations for books.
+ */
 public class BookService {
 
 	private static Map<Long, Book> books = new HashMap<>();
 	private static long nextId = 1;
 
+	/**
+	 * Constructs a new BookService object and generates sample data.
+	 */
 	public BookService() {
 		generateData();
 	}
@@ -33,18 +38,41 @@ public class BookService {
 		return nextId++;
 	}
 
+	/**
+	 * Returns a list of all the books in the HashMap.
+	 * 
+	 * @return A List of all the books in the HashMap.
+	 */
 	public List<Book> getAllBooks() {
 		return new ArrayList<>(books.values());
 	}
 
-	public Book getBookById(String bookId) {
-		return books.get(Long.valueOf(bookId));
+	/**
+	 * Returns a book with the given ID.
+	 * 
+	 * @param bookId The ID of the book to return.
+	 * @return The Book object with the specified ID or null if it does not exist.
+	 */
+	public Book getBookById(Long bookId) {
+		return books.get(bookId);
 	}
 
+	/**
+	 * Checks if a book with the given ID already exists in the HashMap.
+	 * 
+	 * @param id The ID of the book to check.
+	 * @return True if the book exists, false otherwise.
+	 */
 	public boolean keyAlreadyExists(long id) {
 		return books.containsKey(id);
 	}
 
+	/**
+	 * Adds a book to the HashMap.
+	 * 
+	 * @param book The book to add.
+	 * @return The Book object that was added to the HashMap.
+	 */
 	public Book addBook(Book book) {
 		if (book.getId() == 0) {
 			book.setId(getNextId());
@@ -53,6 +81,12 @@ public class BookService {
 		return book;
 	}
 
+	/**
+	 * Updates an existing book in the HashMap.
+	 * 
+	 * @param book The Book object to update.
+	 * @return The Book object that was updated, or null if the book does not exist.
+	 */
 	public Book updateBook(Book book) {
 		if (book == null || !books.containsKey(book.getId())) {
 			return null;
@@ -61,6 +95,11 @@ public class BookService {
 		return book;
 	}
 
+	/**
+	 * Removes a book from the HashMap.
+	 * 
+	 * @param bookId The ID of the book to remove.
+	 */
 	public void deleteBook(Long bookId) {
 		books.remove(bookId);
 	}
